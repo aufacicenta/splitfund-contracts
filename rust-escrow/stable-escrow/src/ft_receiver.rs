71cc9e6b76690c0b7db4529e33317ee78f10873d
Gustavo Ibarra <gus@aufacicenta.com>
2022-10-06 12:03:30 -0600

    refactor(Escrow): implements FungibleTokenReceiver standard

--- rust-escrow/stable-escrow/src/ft_receiver.rs
+++ rust-escrow/stable-escrow/src/ft_receiver.rs
@@ -1,23 +1,17 @@
-use near_sdk::{env, near_bindgen, AccountId, Balance};
+use near_contract_standards::fungible_token::receiver::FungibleTokenReceiver;
+use near_sdk::{env, json_types::U128, near_bindgen, AccountId, PromiseOrValue};
 
 use crate::*;
 
-pub trait FungibleTokenReceiver {
-    // @returns amount of unused tokens
-    fn ft_on_transfer(&mut self, sender_id: AccountId, amount: String, msg: String) -> String;
-}
-
 #[near_bindgen]
 impl FungibleTokenReceiver for Escrow {
-    /**
-     * @notice a callback function only callable by the collateral token for this market
-     * @param sender_id the sender of the original transaction
-     * @param amount of tokens attached to this callback call
-     * @param msg can be a string of any type, in this case we expect a stringified json object
-     * @returns the amount of tokens that were not spent
-     */
     #[payable]
-    fn ft_on_transfer(&mut self, sender_id: AccountId, amount: String, _msg: String) -> String {
+    fn ft_on_transfer(
+        &mut self,
+        sender_id: AccountId,
+        amount: U128,
+        _msg: String,
+    ) -> PromiseOrValue<U128> {
         if !env::state_exists() {
             env::panic_str("ERR_NOT_INITIALIZED");
         }
@@ -26,12 +20,10 @@ impl FungibleTokenReceiver for Escrow {
             env::panic_str("ERR_WRONG_NEP141");
         }
 
-        let amount: Balance = amount.parse::<Balance>().unwrap();
-        assert!(amount > 0, "ERR_ZERO_AMOUNT");
+        assert!(amount.0 > 0, "ERR_ZERO_AMOUNT");
 
-        self.deposit(sender_id, amount);
+        self.deposit(sender_id, amount.0);
 
-        // All the collateral was used, so we should issue no refund on ft_resolve_transfer
-        return "0".to_string();
+        PromiseOrValue::Value(U128(0))
     }
 }
